19f553ae022bb3d5b563db48a1c2676aeb0d1fcf
Federico P. Cortese <cortese.federico@live.it>
2024-07-12 14:15:01 +0200

    mods

--- R/run_par_3and4regimes.R
+++ R/run_par_3and4regimes.R
@@ -76,12 +76,6 @@ save(est2016CA138,elapsed_est2016CA138,file="est2016CA138.RData")
 
 # 2016CO246 ---------------------------------------------------------------
 df2016CO246=read.table("propagation_2016CO246_new_v2.txt",header=T)
-unique(df2016CO246$type)
-
-windows()
-par(mfrow=c(2,1))
-plot(df2016CO246$t,df2016CO246$a,type='l')
-plot(df2016CO246$t,df2016CO246$type,type='l',col='red')
 
 true_states=order_states(df2016CO246$type)
 
@@ -106,11 +100,6 @@ tstheta.stl <- stl(tstheta,
                t.window=NULL,
                na.action = na.approx,
                robust=T)
-
-plot(tstheta.stl)
-
-plot(scale(diff(tstheta.stl$time.series[,2])),type="l")
-lines(scale(true_states[-1]),col="red")
 
 lambda=c(0,5,10,15,20,30)
 kappa=seq(1,ceiling(sqrt(dim(df2016CO246)[2])),by=1)
@@ -142,58 +131,6 @@ unique(df2014OL339$type)
 true_states=order_states(df2014OL339$type)
 
 df2014OL339=df2014OL339[,c("a","e","theta","omega")]
-
-wdn=10
-tsa=ts(df2014OL339$a,frequency = wdn)
-tsa.stl <- stl(tsa, 
-               s.window=wdn,
-               t.window=NULL,
-               na.action = na.approx,
-               robust=T)
-
-plot(tsa.stl)
-
-sdres_a=rollapply(tsa.stl$time.series[,3], 10, sd, fill=NA)
-plot(scale(sdseas_a),type="l")
-lines(scale(true_states),col="red")
-
-tse=ts(df2014OL339$e,frequency = wdn)
-tse.stl <- stl(tse, 
-               s.window=wdn,
-               t.window=NULL,
-               na.action = na.approx,
-               robust=T)
-
-plot(tse.stl)
-
-sdres_e=rollapply(tse.stl$time.series[,3], 10, sd, fill=NA)
-plot(scale(sdseas_e),type="l")
-lines(scale(true_states),col="red")
-
-
-tstheta=ts(df2014OL339$theta,frequency = wdn)
-tstheta.stl <- stl(tstheta, 
-                   s.window=wdn,
-                   t.window=NULL,
-                   na.action = na.approx,
-                   robust=T)
-
-plot(tstheta.stl)
-
-sdseas_theta=rollapply(tstheta.stl$time.series[,1], 10, sd, fill=NA)
-plot(scale(sdseas_theta),type="l")
-
-plot(tstheta.stl$time.series[,3],type="l")
-lines(scale(true_states),col="red")
-
-tsomega=ts(df2014OL339$omega,frequency = wdn)
-tsomega.stl <- stl(tsomega, 
-                   s.window=wdn,
-                   t.window=NULL,
-                   na.action = na.approx,
-                   robust=T)
-
-plot(tsomega.stl)
 
 df2014OL339=compute_feat(df2014OL339,am1=T)
 N=dim(df2014OL339)[1]
@@ -228,31 +165,6 @@ unique(df2020PN1$type)
 true_states=order_states(df2020PN1$type)
 
 df2020PN1=df2020PN1[,c("a","e","theta","omega")]
-
-wdn=10
-tsa=ts(df2020PN1$a,frequency = wdn)
-tsa.stl <- stl(tsa, 
-               s.window=wdn,
-               t.window=NULL,
-               na.action = na.approx,
-               robust=T)
-
-plot(tsa.stl)
-
-tstheta.stl <- stl(tstheta, 
-               s.window=wdn,
-               t.window=NULL,
-               na.action = na.approx,
-               robust=T)
-
-plot(tstheta.stl)
-
-sdseas_theta=rollapply(tstheta.stl$time.series[,1], 10, sd, fill=NA)
-plot(scale(sdseas_theta),type="l")
-
-plot(scale(tstheta.stl$time.series[,1]),type="l")
-lines(scale(true_states),col="red")
-
 
 df2020PN1=compute_feat(df2020PN1,am1=T)
 N=dim(df2020PN1)[1]
